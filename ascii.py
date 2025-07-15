bc0c61ba49992232956840686f9682d2b4a4aace
Lionel <152048641+milckque@users.noreply.github.com>
2025-07-15 17:21:02 +1000

    luminosity calculations added

--- ascii.py
+++ ascii.py
@@ -1,11 +1,17 @@
 from PIL import Image
 import numpy as np
 
-filepath = "IMG_1965.jpeg"
+ASCII_CHARS = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
+
+filepath = "pool1.jpg"
 img = Image.open(filepath)
+img_matrix = np.asarray(img)
 
-# print(f"Image Size = {img.size}")
-# print(f"Image Mode = {img.mode}")
-
-img_array = np.asarray(img)
-print(img_array.shape)
+intensity_matrix = []
+for r in img_matrix:
+    intensity_row = []
+    for c in img_matrix:
+        # luminosity with weighted avgs accounting for human perception
+        intensity = 0.21 * c[0] + 0.72 * c[1] + 0.07 * c[2]
+        intensity_row.append(intensity)
+    intensity_matrix.append(intensity_row)
